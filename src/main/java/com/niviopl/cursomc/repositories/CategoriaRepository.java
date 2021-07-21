8f0374ec07918135264da611652285fa755186e8
nivio lima <nivioplima@yahoo.com>
2021-07-20 22:18:37 -0300

    Criando repository e service - Categoria

---
+++ src/main/java/com/niviopl/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.niviopl.cursomc.repositories;
+
+import com.niviopl.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/niviopl/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.niviopl.cursomc.services;
+
+import com.niviopl.cursomc.domain.Categoria;
+import com.niviopl.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
